68adb312660060490229147ce5004d102bf8f9fe
Stjepan <stipe.milk@gmail.com>
2024-06-15 12:35:26 +0200

    Initialize project and add initial product data

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,82 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <ul v-for="product in products" :key="product.id">
+      <li>
+        <img :src="product.image" :alt="product.title" class="product-image" />
+        <h2>{{ product.title }}</h2>
+        <p>{{ product.description }}</p>
+        <p>{{ product.price }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        {
+          id: "p1",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Books_HD_%288314929977%29.jpg/640px-Books_HD_%288314929977%29.jpg",
+          title: "Book Collection",
+          description:
+            "A collection of must-read books. All-time classics included!",
+          price: 99.99,
+        },
+        {
+          id: "p2",
+          // image:
+          //   "https://upload.wikimedia.org/wikipedia/commons/6/6d/Gongga_Snow_Mountain_Cloud_On_Foot_Mountaineer.jpg",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/e/e6/Wall_tent_in_the_snow_elk_mountain_tents.jpg",
+          title: "Mountain Tent",
+          description: "A tent for the ambitious outdoor tourist.",
+          price: 129.99,
+        },
+        {
+          id: "p3",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/640px-Good_Food_Display_-_NCI_Visuals_Online.jpg",
+          title: "Food Box",
+          description:
+            "May be partially expired when it arrives but at least it is cheap!",
+          price: 6.99,
+        },
+      ],
+    };
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+html {
+  font-family: sans-serif;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+body {
+  margin: 0;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 20px 0;
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+
+img.product-image {
+  width: 640px;
+  height: 426px;
+  object-fit: cover;
 }
 </style>
